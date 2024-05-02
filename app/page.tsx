1b2f0b0d519d76e2100190a566df79c165e1e8a3
Kashaf <akashaf93@gmail.com>
2024-05-03 01:02:53 +0800

    add grid card hover effect component, install packages needed

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { createClient } from "@/utils/supabase/server";
 import { Button, Card, SimpleGrid } from "@mantine/core";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { HoverEffect } from "./components/card-hover-effect";
 
 export default async function Home() {
   const supabase = createClient();
@@ -15,17 +16,12 @@ export default async function Home() {
     return redirect("/login");
   }
 
-  const cards = subject?.map((article) => (
-    <Card key={article.uuid}>
-      <Button component={Link} href={article.file_url}>
-        {article.name}
-      </Button>
-    </Card>
-  ));
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SimpleGrid cols={{ base: 1, sm: 2 }}>{cards}</SimpleGrid>
+      {/* <SimpleGrid cols={{ base: 1, sm: 2 }}>{cards}</SimpleGrid> */}
+      <div className="max-w-5xl mx-auto px-8">
+      {subject && <HoverEffect items={subject} />}  
+    </div>
     </main>
   );
 }
--- app/theme.ts
+++ app/theme.ts
@@ -1,7 +1,21 @@
 "use client";
 
-import { createTheme } from "@mantine/core";
-
+import { MantineColorsTuple, createTheme } from "@mantine/core";
+const myColor: MantineColorsTuple = [
+  '#f7f3f2',
+  '#e7e5e5',
+  '#d2c9c6',
+  '#bdaaa4',
+  '#ab9087',
+  '#a17f75',
+  '#9d766b',
+  '#896459',
+  '#7b584e',
+  '#6d4b40'
+];
 export const theme = createTheme({
   /* Put your mantine theme override here */
+  colors: {
+    myColor,
+  }
 });
