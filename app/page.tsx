ea7d11138534d2ae7f9bde26b99c3c9943ab0fc3
Kashaf <akashaf93@gmail.com>
2024-05-13 00:58:26 +0800

    revert to simplify version grid cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { createClient } from "@/utils/supabase/server";
-import { Button, Card, SimpleGrid } from "@mantine/core";
-import Link from "next/link";
+import { Card, SimpleGrid } from "@mantine/core";
 import { redirect } from "next/navigation";
-import { HoverEffect } from "./components/card-hover-effect";
+import { CardDescription, CardTitle } from "./components/card-hover-effect";
+import Link from "next/link";
 
 export default async function Home() {
   const supabase = createClient();
@@ -16,12 +16,29 @@ export default async function Home() {
     return redirect("/login");
   }
 
+  const cards =
+    subject &&
+    subject.map((item) => (
+      <div key={item.id}>
+        <Link href={item.file_url}>
+          <Card className="transition duration-300 ease-in-out transform hover:scale-105">
+            <CardTitle>{item.name}</CardTitle>
+            <CardDescription>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
+              nihil autem nisi necessitatibus. Temporibus quisquam ab vero rem
+              dolorum mollitia optio reprehenderit placeat molestias aspernatur
+              illo officiis, atque similique ad!
+            </CardDescription>
+          </Card>
+        </Link>
+      </div>
+    ));
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <SimpleGrid cols={{ base: 1, sm: 2 }}>{cards}</SimpleGrid> */}
       <div className="max-w-5xl mx-auto px-8">
-      {subject && <HoverEffect items={subject} />}  
-    </div>
+        {subject && <SimpleGrid cols={{ base: 1, sm: 2 }}>{cards}</SimpleGrid>}
+      </div>
     </main>
   );
 }
