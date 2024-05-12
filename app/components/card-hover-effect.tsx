b5a86d5b79b7712b4ddf130f9aa2990dc0fa1923
Kashaf <akashaf93@gmail.com>
2024-05-13 00:58:05 +0800

    upgrade package and remove unused code

--- app/components/card-hover-effect.tsx
+++ app/components/card-hover-effect.tsx
@@ -1,60 +1,6 @@
 'use client';
 
 import { cn } from "@/utils/cn";
-import { SimpleGrid } from "@mantine/core";
-import { AnimatePresence, motion } from "framer-motion";
-import Link from "next/link";
-import { useState } from "react";
-
-export const HoverEffect = ({
-  items,
-  className,
-}: {
-  items: {
-    name: string;
-    file_url: string;
-  }[];
-  className?: string;
-}) => {
-  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-
-  return (
-    <SimpleGrid
-    cols={{ base: 1, sm: 2 }}>
-      {items.map((item, idx) => (
-        <Link
-          href={item?.file_url}
-          key={item?.file_url}
-          className="relative group  block p-2 h-full w-full"
-          onMouseEnter={() => setHoveredIndex(idx)}
-          onMouseLeave={() => setHoveredIndex(null)}
-        >
-          <AnimatePresence>
-            {hoveredIndex === idx && (
-              <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
-                layoutId="hoverBackground"
-                initial={{ opacity: 0 }}
-                animate={{
-                  opacity: 1,
-                  transition: { duration: 0.15 },
-                }}
-                exit={{
-                  opacity: 0,
-                  transition: { duration: 0.15, delay: 0.2 },
-                }}
-              />
-            )}
-          </AnimatePresence>
-          <Card>
-            <CardTitle>{item.name}</CardTitle>
-            <CardDescription>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum nihil autem nisi necessitatibus. Temporibus quisquam ab vero rem dolorum mollitia optio reprehenderit placeat molestias aspernatur illo officiis, atque similique ad!</CardDescription>
-          </Card>
-        </Link>
-      ))}
-    </SimpleGrid>
-  );
-};
 
 export const Card = ({
   className,
