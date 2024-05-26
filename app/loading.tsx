55715b3119bd6b51598fcf9e48e9a3a4a2dd6a99
Kashaf <akashaf93@gmail.com>
2024-05-26 14:26:55 +0800

    package upgrades and add loading component

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import { Skeleton } from "@mantine/core";
+import { Overlay, Progress } from "@mantine/core";
 
 const LoadingPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Skeleton height={400} width={600} radius="md" />
-      <Skeleton
-        height={40}
-        width={200}
-        radius="sm"
-        style={{ marginLeft: "20px" }}
-      />
-    </div>
+    <Overlay color="#000" backgroundOpacity={0.35} blur={3}>
+      <Progress label="Loading..." indeterminate />
+    </Overlay>
   );
 };
 
