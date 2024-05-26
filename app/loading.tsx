37f8d0421703d55f69efb53fdf6a2c2a341a204c
Kashaf <akashaf93@gmail.com>
2024-05-26 14:38:17 +0800

    refine loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
-import { Overlay, Progress } from "@mantine/core";
+import { Container, Skeleton } from "@mantine/core";
 
 const LoadingPage = () => {
   const [random, setRandom] = useState<number>(0.0);
@@ -10,9 +12,13 @@ const LoadingPage = () => {
   });
 
   return (
-    <Overlay color="#000" backgroundOpacity={0.35} blur={3}>
-      <Progress label="Loading..." indeterminate />
-    </Overlay>
+    <>
+      <Container>
+        <Skeleton height={8} radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} width={`${random}%`} radius="xl" />
+      </Container>
+    </>
   );
 };
 
