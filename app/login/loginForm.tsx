55b0e75defd00ec1024a0ba124c59f01085b7877
Kashaf <akashaf93@gmail.com>
2024-05-27 10:24:58 +0800

    error checking on login form

--- app/login/loginForm.tsx
+++ app/login/loginForm.tsx
@@ -1,33 +1,67 @@
-'use client';
+"use client";
 
-import { useForm } from "react-hook-form";
+import { SubmitHandler, set, useForm } from "react-hook-form";
 import { login } from "./actions";
 import { Button, PasswordInput, TextInput } from "@mantine/core";
 
-export default function LoginForm() {
-    const {handleSubmit, register} = useForm();
+type LoginForm = {
+  email: string;
+  password: string;
+};
 
-  const onSubmit = (data: any) => login(data);
-    return (
-      <>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <TextInput
-            label="Email"
-            {...register("email")}
-            placeholder="akashaf@eduElavate.com"
-            required
-          />
-          <PasswordInput
-            label="Password"
-            {...register("password")}
-            placeholder="Your password"
-            required
-            mt="md"
-          />
-          <Button type="submit" fullWidth mt="xl">
-            Sign in
-          </Button>
-        </form>
-      </>
-    );
+export default function LoginForm() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    setError,
+  } = useForm<LoginForm>();
+
+  const onSubmit: SubmitHandler<LoginForm> = (data) => {
+    try {
+      login(data);
+    } catch (error) {
+      setError("root", {
+        message: "Invalid Data",
+      });
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextInput
+          label="Email"
+          {...register("email", {
+            validate: {
+              email: (value) => value.includes("@") || "Invalid email",
+            },
+          })}
+          placeholder="akashaf@eduElavate.com"
+          error={errors?.email?.message}
+          withAsterisk
+        />
+        <PasswordInput
+          label="Password"
+          {...register("password", {
+            minLength: { value: 8, message: "Password is too short" },
+            validate: {
+              specialChar: (value) =>
+                /[!@#$%^&*]/.test(value) ||
+                "Password must contain special character",
+            },
+          })}
+          placeholder="Your password"
+          mt="md"
+          error={errors.password?.message}
+          withAsterisk
+        />
+        <Button type="submit" fullWidth mt="xl">
+          Sign in
+        </Button>
+        {errors.root && (
+          <div className="text-red-500">{errors.root.message}</div>
+        )}
+      </form>
+    </>
+  );
 }
